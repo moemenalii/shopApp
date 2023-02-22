80a34d9b3d5602688d9d7e827654c620600a08d6
moemen-Ali66 <91580289+moemen-Ali66@users.noreply.github.com>
2023-02-22 16:09:17 +0200

    n

--- lib/CategoriesScreen/CategoriesScreen.dart
+++ lib/CategoriesScreen/CategoriesScreen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-         color:Colors.brown,
-    );
+    return BlocConsumer(
+        builder: (context ,state)=>Scaffold(
+          body: ListView.separated(
+              itemBuilder:(context,state)=>Row(children: [
+
+          ],) ,
+              separatorBuilder:(context,state)=>Container(height: 1,width: double.infinity,) ,
+              itemCount: 20),
+        ),
+        listener: (context ,state){});
   }
 }
